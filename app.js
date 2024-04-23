85691e4070b5da1dd23b6b2b8e856f2104f78f99
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-04-23 15:58:17 -0300

    conceito-nodejs

---
+++ app.js
@@ -0,0 +1,49 @@
+ var Ryan = require("./calculadora");
+
+function mult(a,b){
+    return a * b;
+}
+
+function sub (a,b){
+    return a-b;
+}
+
+function div(a,b){
+    return a/b;
+}
+
+console.log(Ryan(10,20));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var mostrarSite = true;
+const site = "www.guiaprogramador.com";
+
+console.log("Hello World!");
+console.log("meu nome é Ryan!");
+console.log("E eu estou aprendendo Node.js com Guia do programador ");
+
+if(mostrarSite){ // Se mostrar site 
+    console.log(site); // Printe o site 
+} */ 
+
