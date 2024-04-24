d6c93dc6452a004fad263a0462bcbc6dc6096cb2
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-04-24 15:12:24 -0300

    HTTP1

---
+++ HTTP/app.js
@@ -0,0 +1,21 @@
+var http = require("http");
+
+http.createServer(function(requisicao,resposta){
+ resposta.end("<h1>Bem vindo ao meu site!</h1><br><h14>guidoprogramador.com</h4>");
+}).listen(8181);
+
+console.log("Meu sevidor esta rodando!")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
