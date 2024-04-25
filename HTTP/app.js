5e300b4f4af310f79514631a90c9116019fa8164
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-04-25 11:00:02 -0300

    testes modulo OS

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,21 +1,11 @@
 var http = require("http");
+const os = require('os');
+const nomeDoComputador = os.hostname();
+const sOperacional = os.platform();
+
 
 http.createServer(function(requisicao,resposta){
- resposta.end("<h1>Bem vindo ao meu site!</h1><br><h14>udemy.com</h4>");
+ resposta.end("<h1>Bem vindo ao meu site!</h1><br><h14>udemy.com, acessado pelo computador :  "+nomeDoComputador+" no sistema operacional <strong>"+ sOperacional +"</strong></h4>");
 }).listen(8181);
 
 console.log("Meu sevidor esta rodando!")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
