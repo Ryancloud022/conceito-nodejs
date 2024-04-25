f422ac2cc7c2f979f52c288628e1a8ff9ef04136
heronbarroso <heron.barroso@gmail.com>
2024-04-25 10:15:52 -0300

    teste edicao

--- HTTP/app.js
+++ HTTP/app.js
@@ -1,7 +1,7 @@
 var http = require("http");
 
 http.createServer(function(requisicao,resposta){
- resposta.end("<h1>Bem vindo ao meu site!</h1><br><h14>guidoprogramador.com</h4>");
+ resposta.end("<h1>Bem vindo ao meu site!</h1><br><h14>udemy.com</h4>");
 }).listen(8181);
 
 console.log("Meu sevidor esta rodando!")
