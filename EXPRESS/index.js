afb0e1cf08bca674216090dd5c8e375a5ec5a2cb
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-04-25 15:18:46 -0300

    index.js

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -1,11 +1,26 @@
 const  express = require("express"); // Importando  o express
 const app = express(); // Iniciando o express
 
+
+
+app.get("/",function(req,res){
+     res.send("Olá tudo bem?");
+});
+
+app.get("/blog",function(req, res){
+     res.send("Olá Bem Vindo ao blog");
+})
+
+app.get("/canal/youtuber", function(req,res){
+     res.send("<h1>Bem Vindo ao Meu Canal!</h1>");
+})
+
+
 app.listen(8181,function(erro){
   if(erro){
-       console.log("Ocorreu um erro");
+       console.log("Ocorreu um erro!");
   }else{
-    console.log("Servidor iniciando com sucesso!");
+       console.log("Servidor iniciando com sucesso!");
   }
 })
 
