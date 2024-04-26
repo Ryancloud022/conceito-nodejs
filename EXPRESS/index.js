d2ae6364f315fb6c7f994fbedde41e96e7678ce6
Ryancloud022 <ryanpedrozacloud@gmail.com>
2024-04-26 15:59:10 -0300

    index.js

--- EXPRESS/index.js
+++ EXPRESS/index.js
@@ -4,7 +4,7 @@ const app = express(); // Iniciando o express
 
 
 app.get("/",function(req,res){
-     res.send("Olá tudo bem?");
+     res.send("</h1>Olá tudo bem?</h1>");
 });
 
 app.get("/blog",function(req, res){
@@ -12,8 +12,16 @@ app.get("/blog",function(req, res){
 })
 
 app.get("/canal/youtuber", function(req,res){
-     res.send("<h1>Bem Vindo ao Meu Canal!</h1>");
-})
+     res.send("<h3>Bem Vindo ao Meu Canal!</h3>");
+    });
+
+app.get("/ola/:nome/:empresa", function(req, res){
+       // REQ => DADOS ENVIADOS PELO USUÁRIO
+       // RES => RESPOSTA que vai ser enviada para o usuário
+      var nome = req.params.nome; 
+      var empresa = req.params.empresa;
+     res.send("<h1>Oi " + nome +  " do " + empresa +"</h1>");
+});
 
 
 app.listen(8181,function(erro){
